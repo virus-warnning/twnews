fe1afb2c19521b5e1b08ccf1b6a15b58879a632b
Raymond Wu (小璋丸) <virus.warnning@gmail.com>
2018-11-01 11:07:29 +0800

    列舉 beautifulsoup4, requests
    Mentions: #13

--- setup.py
+++ setup.py
@@ -11,16 +11,20 @@ readme.close()
 # https://packaging.python.org/tutorials/packaging-projects/
 # https://python-packaging.readthedocs.io/en/latest/non-code-files.html
 setup(
-  name='twnews',
-  version='0.1.7',
-  description='To tear down news web pages in Taiwan.',
-  long_description=longdesc,
-  packages=find_packages(),
-  url='https://github.com/virus-warnning/twnews',
-  license='MIT',
-  author='Raymond Wu',
-  package_data={
-    'twnews': ['conf/*', 'samples/*']
-  },
-  python_requires='>=3.7'
+    name='twnews',
+    version='0.1.7',
+    description='To tear down news web pages in Taiwan.',
+    long_description=longdesc,
+    packages=find_packages(),
+    url='https://github.com/virus-warnning/twnews',
+    license='MIT',
+    author='Raymond Wu',
+    package_data={
+        'twnews': ['conf/*', 'samples/*']
+    },
+    install_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+    python_requires='>=3.7'
 )
