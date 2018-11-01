cb6ddf715d9cb22c7786809e6a3a57b4d8c600f8
Raymond Wu (小璋丸) <virus.warnning@gmail.com>
2018-11-01 09:45:02 +0800

    支援中時電子報
    Mentions: #4

--- bin/getnews.sh
+++ bin/getnews.sh
@@ -1,12 +1,8 @@
-# 取得新聞網頁離線範本
-#
-# 可以用這 SQL 查詢新聞連結:
-# SELECT id, news FROM unluckyhouse
-# WHERE instr(news, 'www.appledaily.com.tw') ORDER BY id DESC
-
+# 製作新聞離線範本
 SAMPLE_DIR='twnews/samples'
 
 wget -O $SAMPLE_DIR/appledaily.html https://www.appledaily.com.tw/realtimenews/article/local/20160521/867195/%E5%92%8C%E7%94%B7%E5%8F%8B%E5%8F%A3%E8%A7%92%E9%8E%96%E9%96%80%E5%90%9E%E8%97%A5%E3%80%80%E5%A5%B3%E5%A2%9C%E6%A8%93%E4%B8%8D%E6%B2%BB
+wget -O $SAMPLE_DIR/chinatimes.html https://www.chinatimes.com/realtimenews/20180916001767-260402
 wget -O $SAMPLE_DIR/cna.html https://www.cna.com.tw/news/asoc/201603190029-1.aspx
 wget -O $SAMPLE_DIR/ettoday.html https://www.ettoday.net/news/20171209/1069025.htm
 wget -O $SAMPLE_DIR/ltn.html https://news.ltn.com.tw/news/life/breakingnews/2504351
---
+++ twnews/tests/test_chinatimes.py
@@ -0,0 +1,51 @@
+"""
+中時電子報單元測試
+"""
+
+import unittest
+from twnews.soup import NewsSoup, pkgdir
+
+#@unittest.skip
+class TestChinatimes(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'https://www.chinatimes.com/realtimenews/20180916001767-260402'
+        self.dtf = '%Y-%m-%d %H:%M:%S'
+
+    def test_01_sample(self):
+        """
+        測試本地樣本解構
+        * 如果測試 02 失敗，需要用 bin/getnews.sh 重新製作本地樣本
+        """
+        nsoup = NewsSoup(pkgdir + '/samples/chinatimes.html', mobile=False)
+        self.assertEqual('chinatimes', nsoup.channel)
+        self.assertIn('悲慟！北市士林年邁母子 住處上吊自殺身亡', nsoup.title())
+        self.assertEqual('2018-09-16 15:31:00', nsoup.date().strftime(self.dtf))
+        self.assertEqual('謝明俊', nsoup.author())
+        self.assertIn('北市士林區葫蘆街一處民宅', nsoup.contents())
+
+    def test_02_desktop(self):
+        """
+        測試桌面版網頁解構
+        * 務必開啟強制更新，確保解構程式能跟進網站最新版本
+        * 實際新聞內容有可能更新，需要同步單元測試的預期值
+        """
+        nsoup = NewsSoup(self.url, refresh=True, mobile=False)
+        self.assertEqual('chinatimes', nsoup.channel)
+        self.assertIn('悲慟！北市士林年邁母子 住處上吊自殺身亡', nsoup.title())
+        self.assertEqual('2018-09-16 15:31:00', nsoup.date().strftime(self.dtf))
+        self.assertEqual('謝明俊', nsoup.author())
+        self.assertIn('北市士林區葫蘆街一處民宅', nsoup.contents())
+
+    def test_03_mobile(self):
+        """
+        測試行動版網頁解構
+        * 務必開啟強制更新，確保解構程式能跟進網站最新版本
+        * 實際新聞內容有可能更新，需要同步單元測試的預期值
+        """
+        nsoup = NewsSoup(self.url, refresh=True, mobile=True)
+        self.assertEqual('chinatimes', nsoup.channel)
+        self.assertIn('悲慟！北市士林年邁母子 住處上吊自殺身亡', nsoup.title())
+        self.assertEqual('2018-09-16 15:31:00', nsoup.date().strftime(self.dtf))
+        self.assertEqual('謝明俊', nsoup.author())
+        self.assertIn('北市士林區葫蘆街一處民宅', nsoup.contents())
