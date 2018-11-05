8292b737255e93a1c5e32d5cadc00342a3c4d3a3
Raymond Wu (小璋丸) <virus.warnning@gmail.com>
2018-11-05 18:38:53 +0800

    Mentions: #20

--- twnews/__main__.py
+++ twnews/__main__.py
@@ -1,26 +1,30 @@
+import sys
 import os.path
-import logging
-
 from twnews.soup import NewsSoup, pkgdir
 
 def main():
-    samples = [
-        '{}/samples/appledaily.html'.format(pkgdir),
-        'https://tw.news.appledaily.com/local/realtime/20181025/1453825',
-    ]
+    if sys.argv[1].startswith('https'):
+        samples = [sys.argv[1]]
+    else:
+        samples = [
+            '{}/samples/appledaily.html'.format(pkgdir),
+            'https://tw.news.appledaily.com/local/realtime/20181025/1453825',
+        ]
 
-    logging.info('-' * 80)
+    print('-' * 80)
     for path in samples:
         ns = NewsSoup(path, mobile=False)
-        logging.info('路徑: {}'.format(path))
-        logging.info('頻道: {}'.format(ns.channel))
-        logging.info('標題: {}'.format(ns.title()))
-        logging.info('日期: {}'.format(ns.date().isoformat()))
-        logging.info('記者: {}'.format(ns.author()))
-        logging.info('內文:')
-        logging.info(ns.contents())
-        logging.info('有效內容率: {:.2f}%'.format(ns.effective_text_rate() * 100))
-        logging.info('-' * 80)
+        print('路徑: {}'.format(path))
+        print('頻道: {}'.format(ns.channel))
+        print('標題: {}'.format(ns.title()))
+        ndt = ns.date()
+        if ndt is not None:
+            print('日期: {}'.format(ndt.strftime('%Y-%m-%d %H:%M:%S')))
+        print('記者: {}'.format(ns.author()))
+        print('內文:')
+        print(ns.contents())
+        print('有效內容率: {:.2f}%'.format(ns.effective_text_rate() * 100))
+        print('-' * 80)
 
 if __name__ == '__main__':
     main()
