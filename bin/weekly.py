082037832a95693ec7c4d07964803b02e1e9d929
Raymond Wu <virus.warnning@gmail.com>
2018-11-06 22:30:49 +0800

    定期單元測試程式改良
    Mentions: #24

--- bin/weekly.py
+++ bin/weekly.py
@@ -3,11 +3,13 @@
 import json
 import os
 import os.path
+import re
 
 import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 
+# 載入隱私設定
 conf = None
 CONF_PATH = os.path.expanduser('~/.twnews/weekly.json')
 if os.path.isfile(CONF_PATH):
@@ -18,30 +20,32 @@ if conf is None:
     print('Cannot load "{}".'.format(CONF_PATH))
     exit(1)
 
+# 測試
 working_dir = os.path.realpath(os.path.dirname(__file__) + '/..')
 cmd = 'cd {} && python3 -m unittest discover -v 2>&1 1>/dev/null'.format(working_dir)
 pipe = os.popen(cmd, 'r')
 detail = pipe.read()
 ret = pipe.close()
 
+# 組信
 if ret is None:
-    subject = '[twnews] 每週單元測試成功'
-    contents = '''
-        <span style="color:#00f;">每週單元測試成功</span>
-        <br><br>
-        <pre style="border:1px solid #aaa; background:#eee; padding:10px;">
-        {}
-        </pre>
-        '''.format(detail)
+    subject = '[twnews] 每週單元測試 - 成功 ⭕️'
+    contents = re.sub(r'\n\s+\| ', '\n', '''
+        | <h3 style="color:#00f;">每週單元測試成功</h3>
+        | <p>詳細內容：</p>
+        | <pre style="border:1px solid #aaa; background:#eee; padding:10px;">
+        | {}
+        | </pre>
+        ''', re.M).strip().format(detail)
 else:
-    subject = '[twnews] 每週單元測試失敗'
-    contents = '''
-        <span style="color:#f00;">每週單元測試失敗 ({})</span>
-        <br><br>
-        <pre style="border:1px solid #aaa; background:#eee; padding:10px;">
-        {}
-        </pre>
-        '''.format(ret, detail)
+    subject = '[twnews] 每週單元測試 - 失敗 ❌'
+    contents = re.sub(r'\n\s+\| ', '\n', '''
+        | <h3 style="color:#f00;">每週單元測試失敗 ({})</h3>
+        | <p>詳細內容：</p>
+        | <pre style="border:1px solid #aaa; background:#eee; padding:10px;">
+        | {}
+        | </pre>
+        ''', re.M).strip().format(ret, detail)
 
 msg = MIMEText(contents, 'html', 'utf-8')
 msg['Subject'] = Header(subject)
@@ -49,6 +53,7 @@ msg['From'] = '{} <{}>'.format(Header(conf['from_name']).encode(), conf['from_ma
 msg['To'] = '{} <{}>'.format(Header(conf['to_name']).encode(), conf['to_mail'])
 smtp_data = msg.as_string()
 
+# 發信
 try:
     server = smtplib.SMTP(conf['smtp_host'], conf['smtp_port'])
     server.set_debuglevel(1)
@@ -57,4 +62,4 @@ try:
     server.sendmail(conf['from_mail'], conf['to_mail'], smtp_data)
     server.close()
 except Exception as ex:
-    print('Something went wrong.', ex)
+    print('SMTP 異常:', ex)
