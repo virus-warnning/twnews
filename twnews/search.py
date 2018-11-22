630f6f48fe7cb307f90e984b01a5d9c287493f39
Raymond Wu (小璋丸) <virus.warnning@gmail.com>
2018-11-22 12:01:10 +0800

    改善 LINT 問題
    Mentions: #33

--- twnews/search.py
+++ twnews/search.py
@@ -78,20 +78,24 @@ class NewsSearch:
                 raise NewsSearchException(msg)
 
         # 防止開始日期和結束日期格式錯誤
-        self.beg_date = None
-        self.end_date = None
+        self.params = {
+            'beg_date': None,
+            'end_date': None,
+            'channel': channel,
+            'limit': limit
+        }
         try:
             if beg_date is not None:
-                self.beg_date = datetime.strptime(beg_date, '%Y-%m-%d')
+                self.params['beg_date'] = datetime.strptime(beg_date, '%Y-%m-%d')
             if end_date is not None:
-                self.end_date = datetime.strptime(end_date, '%Y-%m-%d')
+                self.params['end_date'] = datetime.strptime(end_date, '%Y-%m-%d')
         except ValueError:
             msg = '日期必須是 ISO 格式 (yyyy-mm-dd)'
             raise NewsSearchException(msg)
 
         # 防止開始日期大於結束日期
-        if self.beg_date is not None:
-            delta = self.end_date - self.beg_date
+        if self.params['beg_date'] is not None:
+            delta = self.params['end_date'] - self.params['beg_date']
             if delta.days < 0:
                 msg = '開始日期必須小於或等於結束日期'
                 raise NewsSearchException(msg)
@@ -99,16 +103,17 @@ class NewsSearch:
                 msg = '頻道 {} 的日期條件必須在 90 天內'.format(channel)
                 raise NewsSearchException(msg)
 
-        self.result_list = []
         self.conf = twnews.common.get_channel_conf(channel, 'search')
-        self.limit = limit
-        self.pages = 0
-        self.elapsed = 0
-        self.host = None
-        self.url_base = None
-        self.soup = None
-        self.json = None
-        self.channel = channel
+        self.context = None
+        self.result = {
+            'pages': 0,
+            'elapsed': 0,
+            'items': []
+        }
+        self.url_prefix = {
+            'host': '',
+            'base': ''
+        }
 
     def by_keyword(self, keyword, title_only=False):
         """
@@ -124,11 +129,11 @@ class NewsSearch:
         # -   支援: 東森、聯合
         # - 不支援: 三立、中央社
         # TODO: 三立、中央社研擬假設 500 頁的二分翻頁法
-        if self.beg_date is not None and self.channel not in ['appledaily', 'ltn', 'setn', 'cna']:
+        if self.params['beg_date'] is not None and self.params['channel'] in ['ettoday', 'udn']:
             # 東森、聯合採取二分翻頁法
             page = self.__flip_to_end_date(keyword)
 
-        while not no_more and len(results) < self.limit:
+        while not no_more and len(results) < self.params['limit']:
             self.__load_page(keyword, page)
 
             # 拆查詢結果
@@ -138,12 +143,13 @@ class NewsSearch:
             if result_count > 0:
                 for node in result_nodes:
                     date_inst = self.__parse_date_node(node)
-                    if self.beg_date is not None and self.channel not in ['appledaily', 'ltn']:
+                    if self.params['beg_date'] is not None and \
+                       self.params['channel'] not in ['appledaily', 'ltn']:
                         # 過濾開頭超過日期範圍的項目
-                        if date_inst > self.end_date:
+                        if date_inst > self.params['end_date']:
                             continue
                         # 過濾結尾超過日期範圍的項目
-                        if date_inst < self.beg_date:
+                        if date_inst < self.params['beg_date']:
                             no_more = True
                             break
 
@@ -155,16 +161,18 @@ class NewsSearch:
                             "link": link,
                             'date': date_inst
                         })
-                        if len(results) == self.limit:
+                        if len(results) == self.params['limit']:
                             break
             else:
                 no_more = True
 
             page += 1
 
-        self.result_list = filter_duplicated(results)
-        self.pages = page - 1
-        self.elapsed = time.time() - begin_time
+        self.result = {
+            'pages': 0,
+            'elapsed': time.time() - begin_time,
+            'items': filter_duplicated(results)
+        }
 
         return self
 
@@ -172,14 +180,14 @@ class NewsSearch:
         """
         回傳新聞查詢結果
         """
-        return self.result_list
+        return self.result['items']
 
     def to_soup_list(self):
         """
         回傳新聞查詢結果的分解器
         """
         soup_list = []
-        for result in self.result_list:
+        for result in self.result['items']:
             nsoup = NewsSoup(result['link'])
             soup_list.append(nsoup)
         return soup_list
@@ -189,64 +197,85 @@ class NewsSearch:
         回傳篩選時間範圍的開始頁數
         """
 
+        page_range = {
+            'lower': 1,
+            'upper': 1000
+        }
+
+        date_range = {
+            'lower': datetime.fromtimestamp(0),
+            'upper': datetime.today()
+        }
+
         # 取得最大頁數
         self.__load_page(keyword, 1)
-        node = self.soup.select(self.conf['last_page'])[0]
+        node = self.context.select(self.conf['last_page'])[0]
         if 'page_pattern' in self.conf:
             match = re.search(self.conf['page_pattern'], node.text)
             if match:
-                last_page = int(match.group(1))
+                page_range['upper'] = int(match.group(1))
             else:
-                last_page = -1
+                page_range['upper'] = -1
         else:
             try:
-                last_page = int(node.text)
+                page_range['upper'] = int(node.text)
             except ValueError:
-                last_page = -1
+                page_range['upper'] = -1
 
-        if last_page == -1:
+        if page_range['upper'] == -1:
             return 1
 
         # 取第一則搜尋結果的日期
-        upper_date = self.__parse_date_node(self.__result_nodes()[0])
-        if upper_date < self.end_date:
+        date_range['upper'] = self.__parse_date_node(self.__result_nodes()[0])
+        if date_range['upper'] < self.params['end_date']:
             return 1
 
         # 取最後一頁最後一則搜尋結果的日期
-        self.__load_page(keyword, last_page)
-        lower_date = self.__parse_date_node(self.__result_nodes()[-1])
-        if lower_date > self.end_date:
+        self.__load_page(keyword, page_range['upper'])
+        date_range['lower'] = self.__parse_date_node(self.__result_nodes()[-1])
+        if date_range['lower'] > self.params['end_date']:
             return -1
 
         # 起始狀態
-        lower_page = 1
-        upper_page = last_page
-        page_range = upper_page - lower_page
-        prev_range = upper_page - lower_page + 1
-        msg = 'page: %d ~ %d, date: %s ~ %s'
+        page_dist = page_range['upper'] - page_range['lower']
+        prev_dist = page_range['upper'] - page_range['lower'] + 1
         logger = twnews.common.get_logger()
-        logger.info(msg, lower_page, upper_page, upper_date, lower_date)
+        logger.info(
+            'page: %d ~ %d, date: %s ~ %s',
+            page_range['lower'],
+            page_range['upper'],
+            date_range['upper'],
+            date_range['lower']
+        )
 
-        while page_range < prev_range:
-            mid_page = (lower_page + upper_page) // 2
+        # 取中間頁數，檢查是否能縮小範圍
+        # 不能縮小範圍時，表示 page_range 的新聞都有 self.params['end_date'] 這一天的
+        while page_dist < prev_dist:
+            mid_page = (page_range['lower'] + page_range['upper']) // 2
             self.__load_page(keyword, mid_page)
             middle_udt = self.__parse_date_node(self.__result_nodes()[0])
             middle_ldt = self.__parse_date_node(self.__result_nodes()[-1])
 
-            if middle_udt > self.end_date:
-                lower_page = mid_page
-                upper_date = middle_udt
+            if middle_udt > self.params['end_date']:
+                page_range['lower'] = mid_page
+                date_range['upper'] = middle_udt
 
-            if middle_ldt < self.end_date:
-                upper_page = mid_page
-                lower_date = middle_ldt
+            if middle_ldt < self.params['end_date']:
+                page_range['upper'] = mid_page
+                date_range['lower'] = middle_ldt
 
-            prev_range = page_range
-            page_range = upper_page - lower_page
-            msg = 'page: %d ~ %d, date: %s ~ %s, mid=%d'
-            logger.info(msg, lower_page, upper_page, upper_date, lower_date, mid_page)
+            prev_dist = page_dist
+            page_dist = page_range['upper'] - page_range['lower']
+            logger.info(
+                'page: %d ~ %d, date: %s ~ %s, mid=%d',
+                page_range['lower'],
+                page_range['upper'],
+                date_range['upper'],
+                date_range['lower'],
+                mid_page
+            )
 
-        return lower_page
+        return page_range['lower']
 
     def __load_page(self, keyword, page):
         # 組查詢條件
@@ -257,9 +286,9 @@ class NewsSearch:
         url = Template(self.conf['url']).substitute(replacement)
 
         # 再加上日期範圍
-        if self.beg_date is not None and self.channel in ['appledaily', 'ltn']:
-            url += self.beg_date.strftime(self.conf['begin_date_format'])
-            url += self.end_date.strftime(self.conf['end_date_format'])
+        if self.params['beg_date'] is not None and self.params['channel'] in ['appledaily', 'ltn']:
+            url += self.params['beg_date'].strftime(self.conf['begin_date_format'])
+            url += self.params['end_date'].strftime(self.conf['end_date_format'])
 
         # 查詢
         session = twnews.common.get_session()
@@ -272,28 +301,25 @@ class NewsSearch:
                 logger.debug('%s: %s', key, val)
             ctype = resp.headers['Content-Type']
             if 'text/html' in ctype:
-                self.soup = BeautifulSoup(resp.text, 'lxml')
+                self.context = BeautifulSoup(resp.text, 'lxml')
             if 'application/json' in ctype:
                 # TODO: 這裡有時會發生 decode error
-                self.json = resp.json()
+                self.context = resp.json()
         elif resp.status_code == 404:
             logger.debug('回應 404 Not Found，視為沒有更多查詢結果')
-            self.json = None
-            # no_more = True
+            self.context = None
         else:
             logger.warning('回應碼: %s', resp.status_code)
-            # break
+            self.context = None
 
     def __result_nodes(self):
         """
         取查詢結果的 soup 或 dict
         """
-        if self.soup is not None:
-            return self.soup.select(self.conf['result_node'])
-
-        if self.json is not None:
-            return visit_dict(self.json, self.conf['result_node'])
-
+        if self.context is not None:
+            if isinstance(self.context, dict):
+                return visit_dict(self.context, self.conf['result_node'])
+            return self.context.select(self.conf['result_node'])
         return []
 
     def __parse_title_node(self, result_node):
@@ -343,23 +369,23 @@ class NewsSearch:
 
         # 絕對路徑
         if href.startswith('/'):
-            if not self.host:
+            if self.url_prefix['host'] == '':
                 match = re.match(r'^https://([^/]+)/', self.conf['url'])
-                self.host = match.group(1)
-            return 'https://{}{}'.format(self.host, href)
+                self.url_prefix['host'] = match.group(1)
+            return 'https://{}{}'.format(self.url_prefix['host'], href)
 
         # 相對路徑
         # 自由的 <head> 有設定 base_url
-        if not self.url_base:
-            nodes = self.soup.select('head > base')
+        if self.url_prefix['base'] == '':
+            nodes = self.context.select('head > base')
             if len(nodes) == 1:
-                self.url_base = nodes[0]['href']
+                self.url_prefix['base'] = nodes[0]['href']
             else:
                 base_end = self.conf['url'].rfind('/')
-                self.url_base = self.conf['url'][0:base_end+1]
+                self.url_prefix['base'] = self.conf['url'][0:base_end+1]
 
         # 消除三立的 ../
-        full_url = self.url_base + href
+        full_url = self.url_prefix['base'] + href
         spos = full_url.find('/', 10)
         reduced_url = full_url[0:spos] + os.path.realpath(full_url[spos:])
 
