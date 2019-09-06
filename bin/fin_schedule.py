0dfadd313b6dc37321922734d90f45b8e220f833
Raymond Wu <raymondwu@fontrip.com>
2019-09-06 18:17:52 +0800

    現有所有排程工作測試成功
    Mentions: #87

--- bin/fin_schedule.py
+++ bin/fin_schedule.py
@@ -4,33 +4,39 @@ import signal
 import sys
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 sys.path.append(os.path.realpath('.'))
 
 import busm
+import twnews.finance.twse as twse
 import twnews.finance.tpex as tpex
+import twnews.finance.tdcc as tdcc
 
-# @busm.through_telegram
-def job(label):
-    # tpex.sync_dataset('institution', '2019-09-02')
-    print('shit')
-    wkd = datetime.now().isoweekday()
-    if wkd > 5:
-        return
-    timestr = datetime.now().strftime('%H:%M:%S.%f')
-    print('[%s] job(%s)' % (timestr, label))
-
-def daemon():
+def run_threaded(task):
     # 觸發排程條件時，平行執行工作，避免超時導致下一個工作被忽略
-    def run_threaded(job_func, *args):
-        th = threading.Thread(target=job_func, args=args)
+    print('Run task %s' % task)
+    trading_date = datetime.now().strftime('%Y-%m-%d')
+    (inst, action) = task.split(':')
+
+    if inst == 'twse':
+        func = twse.sync_dataset
+        args = (action, trading_date)
+    elif inst == 'tpex':
+        func = tpex.sync_dataset
+        args = (action, trading_date)
+    elif inst == 'tdcc':
+        func = tdcc.sync_dataset
+        args = ()
+    else:
+        func = None
+        args = ()
+
+    if func is not None:
+        th = threading.Thread(target=func, args=args)
         th.start()
 
-    def on_quit(signum, frame):
-        nonlocal close_requested
-        close_requested = True
-
+def daemon():
     # 產生 pid file
     pid_file = os.path.expanduser('~/.twnews/fin_schedule.pid')
     pid_base = os.path.dirname(pid_file)
@@ -39,28 +45,34 @@ def daemon():
     with open(pid_file, 'w') as pid_stream:
         pid_stream.write('%s' % os.getpid())
 
-    # kill process 時溫和結束
+    # kill process 時安全結束
     close_requested = False
+
+    def on_quit(signum, frame):
+        nonlocal close_requested
+        close_requested = True
+
     for sig in (signal.SIGTERM, signal.SIGINT, signal.SIGHUP):
         signal.signal(sig, on_quit)
 
-    # schedule.every().day.at('16:52').do(job, 'Test')
+    dt = datetime.now() + timedelta(minutes=1)
+    sch_time = dt.strftime('%H:%M')
 
-    # 證交所
-    schedule.every().day.at('00:30').do(job, '模擬證交所可借券')
-    schedule.every().day.at('08:00').do(job, '模擬證交所ETF淨值')
-    schedule.every().day.at('08:47').do(job, '模擬證交所三大法人')
-    schedule.every().day.at('09:37').do(job, '模擬證交所鉅額交易')
-    schedule.every().day.at('12:44').do(job, '模擬證交所信用交易')
-    schedule.every().day.at('12:45').do(job, '模擬證交所借券賣出')
+    # 證交所 - OK!
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:borrowed')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:etfnet')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:institution')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:block')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:margin')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'twse:selled')
 
-    # 櫃買中心
-    schedule.every().day.at('18:04').do(run_threaded, tpex.sync_dataset, 'institution', '2019-09-02')
-    schedule.every().day.at('09:58').do(job, '模擬櫃買鉅額交易')
-    schedule.every().day.at('12:49').do(job, '模擬櫃買信用交易')
+    # 櫃買中心 - OK!
+    # schedule.every().day.at(sch_time).do(run_threaded, 'tpex:institution')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'tpex:block')
+    # schedule.every().day.at(sch_time).do(run_threaded, 'tpex:margin')
 
     # 集保中心
-    schedule.every().day.at('23:00').do(job, '模擬集保股權分散')
+    schedule.every().day.at(sch_time).do(run_threaded, 'tdcc:')
 
     # 偵測與執行排程
     while not close_requested:
@@ -75,5 +87,5 @@ def daemon():
     os.remove(pid_file)
 
 if __name__ == '__main__':
-    if os.fork() == 0:
-        daemon()
+    # if os.fork() == 0:
+    daemon()
